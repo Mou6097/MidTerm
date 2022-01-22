868f3beb479a8b4bc40eb19af69cc4e54b4754e6
MoJo <MoJo@admins-Air.lan>
2022-01-21 22:21:18 -0500

    First Push

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class gNumbers {
 
-	/* show all the different kind of sort throw sorting like
+	/* show all the different kind of sort throw sorting like applyingg
 	 * Show all the different kind of sorting algorithm by applying into (num array).
 	 * Display the execution time for each sorting.Example in below.
 	 *
